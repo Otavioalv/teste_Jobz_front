a69d950bb38d1ee8b5da6b7daddd4986fe1cb81a
otavioalv <otaviogabriel.alves.barbosa@gmail.com>
2026-03-30 09:09:01 -0400

    feat: configurar estrutura de services para chamadas de API

---
+++ onda_finance/src/features/auth/services/index.tsx
@@ -0,0 +1,12 @@
+// import de conexão ou conexões com API(s)
+
+
+export const fetchLogin = async () => {
+    
+};
+
+export const fetchLogout = async () => {
+
+};
+
+
---
+++ onda_finance/src/services/financeAPI/index.tsx
@@ -0,0 +1,2 @@
+// conexao com api finance, ou uso de bibliotecas (tratamento de respostas de forma agradavel ao usuario)
+// Não implementado
